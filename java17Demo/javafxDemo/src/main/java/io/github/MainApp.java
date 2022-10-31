0ce7b6d5c67ff44b57ac19e108025d06b16653a5
luhua <luhuadream@foxmail.com>
2022-10-31 15:56:30 +0800

    1
    1

---
+++ java17Demo/javafxDemo/src/main/java/io/github/MainApp.java
@@ -0,0 +1,12 @@
+package io.github;
+
+import io.github.view.MainView;
+import javafx.application.Application;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        Application.launch(MainView.class,args);
+    }
+
+}
---
+++ java17Demo/javafxDemo/src/main/java/io/github/view/MainView.java
@@ -0,0 +1,14 @@
+package io.github.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello world");
+        primaryStage.show();
+    }
+
+}
